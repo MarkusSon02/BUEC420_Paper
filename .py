d7fa9d37aa246651392878a42919914da256cfc0
Son Tran <sonthanhtran2004@gmail.com>
2025-03-22 22:25:02 -0600

    Joined and Cleaned data, added other European countries to the lists of data
    Before it was just EU countries

--- .py
+++ .py
@@ -11,18 +11,17 @@ WDI_df = WDI_df.rename({"Time": "Year"}, axis=1)
 GEM_df = GEM_df.reset_index()
 WDI_df = WDI_df.reset_index()
 
-
 GEM_df['Year'] = GEM_df['Time'].str[:4]
-GEM_df['Year'] = GEM_df['Year'].astype(str).str.strip()
-GEM_df['Country Code'] = GEM_df['Country Code'].astype(str).str.strip().str.upper()
-
-WDI_df['Year'] = WDI_df['Year'].astype(str).str.strip()
-WDI_df['Country Code'] = WDI_df['Country Code'].astype(str).str.strip().str.upper()
+GEM_df['Time'] = GEM_df['Time'].astype(str)
+GEM_df["Year"] = pd.to_numeric(GEM_df["Year"], errors='coerce')
+GEM_df['Country Code'] = GEM_df['Country Code'].astype(str)
+WDI_df["Year"] = pd.to_numeric(WDI_df["Year"], errors='coerce')
+WDI_df['Country Code'] = WDI_df['Country Code'].astype(str)
 
 df = pd.merge(GEM_df, WDI_df, on=["Year", "Country Code"], how="left")
 
-print(GEM_df[['Year', 'Country Code']].dtypes)
-print(WDI_df.reset_index()[['Year', 'Country Code']].dtypes)
+# print(GEM_df[['Year', 'Country Code']].dtypes)
+# print(WDI_df.reset_index()[['Year', 'Country Code']].dtypes)
 
 # print("GEM_df index:", GEM_df.index)
 # print("GEM_df columns:", GEM_df.columns)
@@ -30,5 +29,37 @@ print(WDI_df.reset_index()[['Year', 'Country Code']].dtypes)
 # print("WDI_df index:", WDI_df.index)
 # print("WDI_df columns:", WDI_df.columns)
 
+count_countries = df["Country Code"].value_counts()
+print(len(count_countries))
 display(df.head(3))
 # df.to_excel("data/EU_Joined_data.xlsx")
+
+
+for col in df.columns:
+    if col not in ["Time", "Country Code"]:
+        df[col] = pd.to_numeric(df[col], errors='coerce')
+
+df['Time'] = df['Time'].astype("string")
+df['Country Code'] = df['Country Code'].astype("string")
+
+
+df = df[~df['Country Code'].isin(['EST', 'MLT', 'LTU'])]
+df['Year'] = pd.to_numeric(df['Year'], errors='coerce')
+df = df[df['Year'] > 1993]
+df = df[df['Time'] < "2024M04"]
+df = df.sort_values(by=["Country Code", "Time"])
+
+
+
+print(df.isna().sum().sum())
+
+# count_countries = df["Country Code"].value_counts()
+# print(len(count_countries))
+
+df = df.groupby(["Country Code"], as_index=False).ffill()
+df = df.reset_index()
+print(df.dtypes)
+df.to_excel("data/EU_cleaned_data.xlsx", index=False)
+
+print(df.isna().sum().sum())
+display(df.head(3))
