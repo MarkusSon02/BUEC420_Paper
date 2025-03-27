9a4619b622011deced7ebe08ab4a6b27008cc9bb
Son Tran <sonthanhtran2004@gmail.com>
2025-03-27 00:34:40 -0600

    new countries added to test
    Some variables were removed to accommodate the new countries

--- .py
+++ .py
@@ -8,8 +8,8 @@ import matplotlib.pyplot as plt
 GEM_df = pd.read_excel("data/EU_GEM_data.xlsx")
 WDI_df = pd.read_excel("data/EU_WDI_data.xlsx")
 
-GEM_df = GEM_df.drop(columns=['Time Code', 'CPI Price, % y-o-y, median weighted, seas. adj., [CPTOTSAXMZGY]'])
-WDI_df = WDI_df.drop(columns=['Time Code', 'Country Name', "Educational attainment, at least Master's or equivalent, population 25+, total (%) (cumulative) [SE.TER.CUAT.MS.ZS]", "Educational attainment, Doctoral or equivalent, population 25+, total (%) (cumulative) [SE.TER.CUAT.DO.ZS]"])
+GEM_df = GEM_df.drop(columns=['Time Code', 'CPI Price, % y-o-y, median weighted, seas. adj., [CPTOTSAXMZGY]', 'Exports Merchandise, Customs, current US$, millions, seas. adj. [DXGSRMRCHSACD]', 'Imports Merchandise, Customs, current US$, millions, seas. adj. [DMGSRMRCHSACD]', 'Industrial Production, constant US$, seas. adj.,, [IPTOTSAKD]'])
+WDI_df = WDI_df.drop(columns=['Time Code', 'Country Name', "Educational attainment, at least Master's or equivalent, population 25+, total (%) (cumulative) [SE.TER.CUAT.MS.ZS]", "Educational attainment, Doctoral or equivalent, population 25+, total (%) (cumulative) [SE.TER.CUAT.DO.ZS]", "Educational attainment, at least Bachelor's or equivalent, population 25+, total (%) (cumulative) [SE.TER.CUAT.BA.ZS]", "Educational attainment, at least completed lower secondary, population 25+, total (%) (cumulative) [SE.SEC.CUAT.LO.ZS]", "Educational attainment, at least completed post-secondary, population 25+, total (%) (cumulative) [SE.SEC.CUAT.PO.ZS]", "Educational attainment, at least completed primary, population 25+ years, total (%) (cumulative) [SE.PRM.CUAT.ZS]", "Educational attainment, at least completed short-cycle tertiary, population 25+, total (%) (cumulative) [SE.TER.CUAT.ST.ZS]", "Educational attainment, at least completed upper secondary, population 25+, total (%) (cumulative) [SE.SEC.CUAT.UP.ZS]",])
 WDI_df = WDI_df.rename({"Time": "Year"}, axis=1)
 
 GEM_df = GEM_df.reset_index(drop=True)
@@ -50,6 +50,7 @@ joined_df['Country'] = joined_df['Country'].astype("string")
 
 
 joined_df = joined_df[~joined_df['Country Code'].isin(['EST', 'MLT', 'LTU', 'HRV', 'CYP', 'CZE', 'GRC', 'SVK', 'AUT', 'FRA', 'HUN', 'YUG', 'CHE', 'GEO', 'RUS', 'BGR', 'SVN', 'DNK', 'ISL', 'GBR'])]
+joined_df = joined_df[~joined_df['Country Code'].isin(['ARE', 'ARG', 'AUS', 'BRA', 'JPN', 'MYS', 'NZL', 'PHL'])]
 joined_df['Year'] = pd.to_numeric(joined_df['Year'], errors='coerce')
 joined_df = joined_df[joined_df['Year'] > 2009]
 joined_df = joined_df[joined_df['Year'] < 2019]
@@ -77,30 +78,30 @@ joined_df.to_excel("data/EU_cleaned_data.xlsx", index=False)
 print(joined_df.isna().sum().sum())
 # print("\n\n\n\n\n")
 print(joined_df.shape)
-# print(joined_df.groupby("Country Code").apply(lambda x: x.isnull().sum().sum()))
+print(joined_df.groupby("Country Code").apply(lambda x: x.isnull().sum().sum()))
 # display(joined_df.head(3))
 
 analysis_df = joined_df
 
 # Calculate net exports if not already computed
 # analysis_df.columns = ['index', 'Time', 'Country', 'Country Code', 'CPI Price', 'Exports, not seas. adj', 'Exports, seas. adj', 'Imports, not seas. adj', 'Imports, seas. adj', 'Industrial Production, not seas. adj', 'Industrial Production, seas. adj', 'Official exchange rate', 'Year', 'Labor force', 'Labor force participation rate ILO estimate', 'Labor force participation rate national estimate', 'Labor force with advanced education', 'Labor force with basic education', 'Labor force with intermediate education', ]
-analysis_df['Net Exports seas. adj'] = analysis_df['Exports Merchandise, Customs, current US$, millions, seas. adj. [DXGSRMRCHSACD]'] - analysis_df['Imports Merchandise, Customs, current US$, millions, seas. adj. [DMGSRMRCHSACD]']
+# analysis_df['Net Exports seas. adj'] = analysis_df['Exports Merchandise, Customs, current US$, millions, seas. adj. [DXGSRMRCHSACD]'] - analysis_df['Imports Merchandise, Customs, current US$, millions, seas. adj. [DMGSRMRCHSACD]']
 analysis_df['Net Exports not seas. adj'] = analysis_df['Exports Merchandise, Customs, current US$, millions, not seas. adj. [DXGSRMRCHNSCD]'] - analysis_df['Imports Merchandise, Customs, current US$, millions, not seas. adj. [DMGSRMRCHNSCD]']
-analysis_df['Lag Net Exports seas. adj'] = analysis_df.groupby("Country Code")['Net Exports seas. adj'].shift(1)
+# analysis_df['Lag Net Exports seas. adj'] = analysis_df.groupby("Country Code")['Net Exports seas. adj'].shift(1)
 analysis_df['Lag Net Exports not seas. adj'] = analysis_df.groupby("Country Code")['Net Exports not seas. adj'].shift(1)
 analysis_df["Official exchange rate percent change"] = analysis_df.groupby("Country Code")["Official exchange rate, LCU per USD, period average,, [DPANUSLCU]"].pct_change() * 100
 analysis_df["Nominal Effective Exchange Rate percent change"] = analysis_df.groupby("Country Code")["Nominal Effective Exchange Rate,,,, [NEER]"].pct_change() * 100
 analysis_df["Real Effective Exchange Rate percent change"] = analysis_df.groupby("Country Code")["Real Effective Exchange Rate,,,, [REER]"].pct_change() * 100
 analysis_df["Net Exports percent change"] = analysis_df.groupby("Country Code")["Net Exports not seas. adj"].pct_change() * 100
 analysis_df.reset_index()
-analysis_df = analysis_df.dropna(subset=['Lag Net Exports seas. adj', 'Lag Net Exports not seas. adj', "Official exchange rate percent change"])
+# analysis_df = analysis_df.dropna(subset=['Lag Net Exports seas. adj', 'Lag Net Exports not seas. adj', "Official exchange rate percent change"])
 
 # analysis_df['ln_Net_Exports seas. adj'] = np.log(analysis_df['Net Exports seas. adj'])
 # analysis_df['ln_Net_Exports not seas. adj'] = np.log(analysis_df['Net Exports not seas. adj'])
 # analysis_df['ln_Lag_Net_Exports seas.adj'] = np.log(analysis_df['Lag Net Exports seas. adj'])
 # analysis_df['ln_Lag_Net_Exports not seas. adj'] = np.log(analysis_df['Lag Net Exports not seas. adj'])
 analysis_df['ln_Labor'] = np.log(analysis_df['Labor force, total [SL.TLF.TOTL.IN]'])
-analysis_df['ln_Industial_Production seas. adj'] = np.log(analysis_df['Industrial Production, constant US$, seas. adj.,, [IPTOTSAKD]'])
+# analysis_df['ln_Industial_Production seas. adj'] = np.log(analysis_df['Industrial Production, constant US$, seas. adj.,, [IPTOTSAKD]'])
 analysis_df['ln_Industial_Production not seas. adj'] = np.log(analysis_df['Industrial Production, constant US$,,, [IPTOTNSKD]'])
 analysis_df['ln_GDP'] = np.log(analysis_df['GDP (current US$) [NY.GDP.MKTP.CD]'])
 analysis_df['ln_GDP_per_capita'] = np.log(analysis_df['GDP per capita (current US$) [NY.GDP.PCAP.CD]'])
@@ -145,7 +146,7 @@ analysis_df = analysis_df[(analysis_df['Official exchange rate percent change'] 
 
 # Specify the independent variables.
 # Adjust the variable names to match your actual DataFrame columns.
-exog_vars = ['Lag Net Exports not seas. adj', "Real Effective Exchange Rate percent change", "Official exchange rate percent change", 'CPI Price, % y-o-y, not seas. adj.,, [CPTOTSAXNZGY]', 'ln_Industial_Production not seas. adj', 'ln_Labor', "GDP growth (annual %) [NY.GDP.MKTP.KD.ZG]", "GDP per capita growth (annual %) [NY.GDP.PCAP.KD.ZG]", "Educational attainment, at least Bachelor's or equivalent, population 25+, total (%) (cumulative) [SE.TER.CUAT.BA.ZS]", "Educational attainment, at least completed lower secondary, population 25+, total (%) (cumulative) [SE.SEC.CUAT.LO.ZS]", "Educational attainment, at least completed post-secondary, population 25+, total (%) (cumulative) [SE.SEC.CUAT.PO.ZS]", "Educational attainment, at least completed primary, population 25+ years, total (%) (cumulative) [SE.PRM.CUAT.ZS]", "Educational attainment, at least completed short-cycle tertiary, population 25+, total (%) (cumulative) [SE.TER.CUAT.ST.ZS]", "Educational attainment, at least completed upper secondary, population 25+, total (%) (cumulative) [SE.SEC.CUAT.UP.ZS]", "Winter", "Spring", "Summer"]
+exog_vars = ['Lag Net Exports not seas. adj', "Real Effective Exchange Rate percent change", "Official exchange rate percent change", 'CPI Price, % y-o-y, not seas. adj.,, [CPTOTSAXNZGY]', 'ln_Industial_Production not seas. adj', 'ln_Labor', "GDP growth (annual %) [NY.GDP.MKTP.KD.ZG]", "GDP per capita growth (annual %) [NY.GDP.PCAP.KD.ZG]", "Winter", "Spring", "Summer"]
 
 # # print("STD and Corr 1:")
 # # print(analysis_df.groupby("Country Code")[exog_vars].std())
@@ -167,7 +168,7 @@ model = PanelOLS(
 results = model.fit(cov_type='clustered', cluster_entity=True)
 print(results.summary)
 
-exog_vars_2 = ['Lag Net Exports not seas. adj', "Real Effective Exchange Rate,,,, [REER]", "Official exchange rate, LCU per USD, period average,, [DPANUSLCU]", 'CPI Price, % y-o-y, not seas. adj.,, [CPTOTSAXNZGY]', 'ln_Labor', "GDP growth (annual %) [NY.GDP.MKTP.KD.ZG]", "GDP per capita growth (annual %) [NY.GDP.PCAP.KD.ZG]", "Educational attainment, at least Bachelor's or equivalent, population 25+, total (%) (cumulative) [SE.TER.CUAT.BA.ZS]", "Educational attainment, at least completed lower secondary, population 25+, total (%) (cumulative) [SE.SEC.CUAT.LO.ZS]", "Educational attainment, at least completed post-secondary, population 25+, total (%) (cumulative) [SE.SEC.CUAT.PO.ZS]", "Educational attainment, at least completed primary, population 25+ years, total (%) (cumulative) [SE.PRM.CUAT.ZS]", "Educational attainment, at least completed short-cycle tertiary, population 25+, total (%) (cumulative) [SE.TER.CUAT.ST.ZS]", "Educational attainment, at least completed upper secondary, population 25+, total (%) (cumulative) [SE.SEC.CUAT.UP.ZS]", "Exports of goods and services (% of GDP) [NE.EXP.GNFS.ZS]", "Imports of goods and services (% of GDP) [NE.IMP.GNFS.ZS]", "Unemployment, total (% of total labor force) (modeled ILO estimate) [SL.UEM.TOTL.ZS]"]
+exog_vars_2 = ['Lag Net Exports not seas. adj', "Real Effective Exchange Rate,,,, [REER]", "Official exchange rate, LCU per USD, period average,, [DPANUSLCU]", 'CPI Price, % y-o-y, not seas. adj.,, [CPTOTSAXNZGY]', 'ln_Labor', "GDP growth (annual %) [NY.GDP.MKTP.KD.ZG]", "GDP per capita growth (annual %) [NY.GDP.PCAP.KD.ZG]", "Exports of goods and services (% of GDP) [NE.EXP.GNFS.ZS]", "Imports of goods and services (% of GDP) [NE.IMP.GNFS.ZS]", "Unemployment, total (% of total labor force) (modeled ILO estimate) [SL.UEM.TOTL.ZS]","Winter", "Spring", "Summer"]
 
 # with open("results/STD_Corr_2.txt", "w") as f:
 #     f.write("Standard Deviation by Country:\n")
@@ -197,7 +198,7 @@ model_2 = PanelOLS(
 results_2 = model_2.fit(cov_type='clustered', cluster_entity=True)
 print(results_2.summary)
 
-exog_vars_3 = ['Lag Net Exports not seas. adj', "Real Effective Exchange Rate,,,, [REER]", "Official exchange rate, LCU per USD, period average,, [DPANUSLCU]", 'CPI Price, % y-o-y, not seas. adj.,, [CPTOTSAXNZGY]', 'ln_Labor', "GDP growth (annual %) [NY.GDP.MKTP.KD.ZG]", "GDP per capita growth (annual %) [NY.GDP.PCAP.KD.ZG]", "Educational attainment, at least Bachelor's or equivalent, population 25+, total (%) (cumulative) [SE.TER.CUAT.BA.ZS]", "Exports of goods and services (% of GDP) [NE.EXP.GNFS.ZS]", "Imports of goods and services (% of GDP) [NE.IMP.GNFS.ZS]", "Unemployment, total (% of total labor force) (modeled ILO estimate) [SL.UEM.TOTL.ZS]"]
+exog_vars_3 = ['Lag Net Exports not seas. adj', "Real Effective Exchange Rate,,,, [REER]", "Official exchange rate, LCU per USD, period average,, [DPANUSLCU]", 'CPI Price, % y-o-y, not seas. adj.,, [CPTOTSAXNZGY]', 'ln_Labor', "GDP growth (annual %) [NY.GDP.MKTP.KD.ZG]", "GDP per capita growth (annual %) [NY.GDP.PCAP.KD.ZG]", "Exports of goods and services (% of GDP) [NE.EXP.GNFS.ZS]", "Imports of goods and services (% of GDP) [NE.IMP.GNFS.ZS]", "Unemployment, total (% of total labor force) (modeled ILO estimate) [SL.UEM.TOTL.ZS]","Winter", "Spring", "Summer"]
 
 with open("results/STD_Corr_2.txt", "w") as f:
     f.write("Standard Deviation by Country:\n")
@@ -227,10 +228,10 @@ model_3 = PanelOLS(
 results_3 = model_3.fit(cov_type='clustered', cluster_entity=True)
 print(results_3.summary)
 
-# with open("results/analysis_summary.txt", "w") as f:
-#     f.write(results.summary.as_text())
-# with open("results/analysis_summary_2.txt", "w") as f:
-#     f.write(results_2.summary.as_text())
+with open("results/analysis_summary.txt", "w") as f:
+    f.write(results.summary.as_text())
+with open("results/analysis_summary_2.txt", "w") as f:
+    f.write(results_2.summary.as_text())
 with open("results/analysis_summary_3.txt", "w") as f:
     f.write(results_3.summary.as_text())
 
